06b57fd49c744a0bad7356e433dd53938f2ae42a
Renato de souza lucas <42358704+renatodesouza@users.noreply.github.com>
2025-07-06 15:37:18 -0300

    feat: campo foto alterado para ImageField na model Fotografia

--- galeria/models.py
+++ galeria/models.py
@@ -4,7 +4,7 @@ class Fotografia(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=100, null=False, blank=False)
     descricao = models.TextField(null=False, blank=False)
-    foto = models.CharField(max_length=100, null=False, blank=False)
+    foto = models.ImageField(upload_to='fotos/%y%m%d')
 
     def __str__(self):
         return f"Fotografia [nome={self.nome}]"
